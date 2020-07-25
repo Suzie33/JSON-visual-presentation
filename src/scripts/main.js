d79873ef973df5efa2064ff5c1f082799e2b6d2b
Valentina Ponamareva <valiazin@gmail.com>
2020-07-25 13:30:42 +0300

    implemented visual presentation of data in textarea

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,40 @@
 (function() {
   const inputTextarea = document.querySelector('#textarea_input');
   const startButton = document.querySelector('#button-start');
-  const outputTree = document.querySelector('#output__tree');
-  let inputData = '';
+  const outputTree = document.querySelector('#output');
 
   inputTextarea.addEventListener('click', function() {
     inputTextarea.textContent = '';
   })
 
   startButton.addEventListener('click', function() {
-    let inputParsedData = '';
+    const inputData = inputTextarea.value;
+    const inputParsedData = parseData(inputData);
 
-    inputData = inputTextarea.value;
-    inputParsedData = JSON.parse(inputData);
+    createTreeBranch(inputParsedData, outputTree);
+  })
 
-    for (let [key, value] of Object.entries(inputParsedData)) {
+  function parseData(string) {
+    return JSON.parse(string);
+  }
+
+  function createTreeBranch(obj, container) {
+    const treeBranch = document.createElement('ul');
+    treeBranch.classList.add('output__tree');
+    container.appendChild(treeBranch);
+    
+    for (let [key, value] of Object.entries(obj)) {
+      
       const treeItem = document.createElement('li');
       treeItem.classList.add('output__tree-item');
       treeItem.textContent = `"${key}": "${value}"`;
-      outputTree.appendChild(treeItem);
+      treeBranch.appendChild(treeItem);
+
+      if (typeof value === 'object') {
+        treeItem.textContent = `"${key}":`;
+        createTreeBranch(value, treeItem);
+      } 
     }
-  })
+  }
 
 }());
